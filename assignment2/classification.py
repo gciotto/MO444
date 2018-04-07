48ae5e3f40ab51c0e42badfb1beb9883b5142c3d
Gustavo Ciotto <gustavociotto@gmail.com>
2018-04-07 17:38:56 -0300

    multi threading programming

--- assignment2/classification.py
+++ assignment2/classification.py
@@ -1,6 +1,7 @@
-import os
 import cv2
 import matplotlib.pyplot
+import os
+import threading
 
 class Classifier ():
 
@@ -9,25 +10,30 @@ class Classifier ():
         self.trainingAddr = trainingAddr
         self.testAddr = testAddr
 
+    def extractFeatureThread (self, subdir):
+
+        for file in os.listdir (self.trainingAddr + "/" + subdir)[0:1]:
+
+            print (self.trainingAddr + "/" + subdir + "/" + file)
+
+            h = 5
+            templateWindowSize = 7
+            searchWindowSize = 21
+
+            noisedImg = cv2.imread (self.trainingAddr + "/" + subdir + "/" + file)
+
+            denoisedPath = "%s/%s/%s_%i_%i_%i_%i" % (self.trainingAddr, subdir, file, h, h, templateWindowSize, searchWindowSize)
+
+            if os.path.isfile (denoisedPath):
+                denoisedImg = cv2.imread (denoisedPath)
+            else:
+                denoisedImg = cv2.fastNlMeansDenoisingColored (noisedImg, None, h, h, templateWindowSize , searchWindowSize)
+                cv2.imwrite (denoisedPath, denoisedImg)
+
+            noise = noisedImg - denoisedImg
+
     def extractFeatures (self):
 
         for subdir in os.listdir (self.trainingAddr):
-            for file in os.listdir (self.trainingAddr + "/" + subdir):
-
-                print (self.trainingAddr + "/" + subdir + "/" + file)
-
-                h = 5
-                templateWindowSize = 7
-                searchWindowSize = 21
-
-                noisedImg = cv2.imread (self.trainingAddr + "/" + subdir + "/" + file)
-
-                denoisedPath = "%s/%s/%s_%i_%i_%i_%i" % (self.trainingAddr, subdir, file, h, h, templateWindowSize, searchWindowSize)
-
-                if os.path.isfile (denoisedPath):
-                    denoisedImg = cv2.imread (denoisedPath)
-                else:
-                    denoisedImg = cv2.fastNlMeansDenoisingColored (noisedImg, None, h, h, templateWindowSize , searchWindowSize)
-                    cv2.imwrite (denoisedPath, denoisedImg)
-
-                noise = noisedImg - denoisedImg
+            t = threading.Thread (target = self.extractFeatureThread, args = (subdir,))
+            t.start ()
